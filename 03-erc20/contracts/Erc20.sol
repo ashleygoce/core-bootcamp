a3f51e3a60573f8dbf6dca90943b57cc846e2843
ashleygoce <ashley.denise.goce@adamson.edu.ph>
2025-02-28 10:27:00 +0800

    first commit

--- 03-erc20/contracts/Erc20.sol
+++ 03-erc20/contracts/Erc20.sol
@@ -2,12 +2,12 @@
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NameYourToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("NameYourToken", "NYT") {
-        // _mint(msg.sender, initialSupply);
+contract ZERI2 is ERC20 {
+    constructor(uint256 initialSupply) ERC20("ZERI2", "ZR2") {
+         _mint(msg.sender, initialSupply);
     }
 
     function generateTokens(address destination, uint256 amount) public {
-        // _mint(destination, amount);s
+         _mint(destination, amount);
     }
 }
